10d07548e0a04d69c57a3832c9fe1fdd96970c49
Cesar Febres <febrescesar7@gmail.com>
2024-03-18 23:54:57 -0400

    toggle, roles and layout

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,16 +2,15 @@
   <q-list>
     <q-item-label header> Menu </q-item-label>
 
-    <EssentialLink
-      v-for="link in essentialLinks"
-      :key="link.title"
-      v-bind="link"
-    />
+    <EssentialLink v-for="link in menuToShow" :key="link.title" v-bind="link" />
   </q-list>
 </template>
 <script>
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, watch, onMounted } from 'vue';
 import EssentialLink from 'components/EssentialLink.vue';
+import { useUserStore } from '../stores/user';
+import { storeToRefs } from 'pinia';
+
 const menuRecruiters = [
   {
     title: 'Applicants',
@@ -87,10 +86,40 @@ export default defineComponent({
   components: {
     EssentialLink,
   },
+
   setup() {
+    const userStore = useUserStore();
+    const { currentRole } = storeToRefs(userStore)
+
+    const menuToShow = ref([]);
+
+    const handleMenuOptions = () => {
+      if (currentRole.value == 'recruiter') {
+        menuToShow.value = menuRecruiters;
+      } else {
+        menuToShow.value = menuApplicants;
+      }
+    }
+
+    watch(currentRole, () => {
+      console.log('currentRole state changed, do something!');
+      handleMenuOptions();
+    });
+
+    onMounted(() => {
+      handleMenuOptions();
+    });
+
     return {
-      essentialLinks: menuApplicants,
+      menuToShow,
+      currentRole,
+      userStore
     };
+  },
+  computed: {
+    tripleCounter() {
+      return this.counterStore.counter * 3
+    },
   },
 });
 </script>
--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,22 +1,27 @@
 import { defineStore } from 'pinia';
-import { useProfileStore } from './profile'
+import { useProfileStore } from './profile';
 import axios from 'axios';
 
 export const useUserStore = defineStore('counter', {
   state: () => ({
-    userData: null // Agregar un state para almacenar los datos del usuario
+    userData: null, // Agregar un state para almacenar los datos del usuario
+    currentRole: 'recruiter', // recruiter o applicant
+    premiun: false, // TODO: pa despues
   }),
 
   getters: {
-    doubleCount (state) {
-      return state.counter * 2;
-    }
+    getCurrentRole(): boolean {
+      return this.currentRole === 'recruiter' ? true : false;
+    },
   },
 
   actions: {
-    async fetchUserData(userId:string) {
+    async fetchUserData(userId: string) {
       try {
-        const response = await axios.get(`https://getuserbyauth0route-7mlffi3t2a-uc.a.run.app?id=${userId}`);
+        // TODO: remover luego
+        const response = await axios.get(
+          `https://getuserbyauth0route-7mlffi3t2a-uc.a.run.app?id=${userId}`
+        );
         this.userData = response.data; // Almacena los datos del usuario en el state userData
 
         // Accede al store de profile y actualiza los datos del usuario
@@ -26,9 +31,12 @@ export const useUserStore = defineStore('counter', {
         console.error(error);
       }
     },
-
-    increment () {
-      this.counter++;
-    }
-  }
+    setRole(role: boolean) {
+      if (role) {
+        this.currentRole = 'recruiter';
+      } else {
+        this.currentRole = 'applicant';
+      }
+    },
+  },
 });
