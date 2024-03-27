e9f79d00575422e715d5f3afd4b4bc47a87afed0
Cesar Febres <febrescesar7@gmail.com>
2024-03-26 23:20:06 -0400

    landinv v2

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,118 +1,172 @@
 <template>
-  <q-layout>
-    <q-page-container class="q-pa-md">
-      <q-page class="my-padding-x q-ml-sm q-pa-xl">
-        <div class="row q-col-gutter-md justify-center">
-          <div class="col-md-9 col-xs-12">
-            <q-card class="my-card">
-              <img src="../assets/iqjobs.png" height="230">
-              <q-card-section class="row justify-around">
-                <div class="text-h3 col-sm-5 col-xs-12 text-weight-bold">Welcome to the IQJobs platform.</div>
+  <div class="">
+    <q-layout style="height: 47.7vw" class="shadow-2 rounded-borders">
+      <q-header>
+        <q-toolbar>
+          <q-avatar>
+            <img src="../assets/logotipo.png" alt="logo" />
+          </q-avatar>
 
-                <div class="q-gutter-sm col-md-5 col-xs-12">
-                  <div class="text-caption">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quasi eaque
-                    illo unde ratione, explicabo cum iste dolore, saepe neque, mollitia maxime. Non deserunt labore
-                    nostrum ad facilis excepturi maiores!</div>
-                  <q-btn class="bg-primary text-white q-mt-lg" label="Log In" unelevated flat />
-                </div>
-              </q-card-section>
-            </q-card>
+          <q-toolbar-title>IQ Jobs</q-toolbar-title>
+
+          <q-btn id="btn-early-access" unelevated rounded color="secondary" class="text-white" label="Early Access"
+            @click="handleRedirect" />
+        </q-toolbar>
+
+      </q-header>
+
+      <q-page-container style="height: 100%;">
+        <div class="container flex-center" style="height: 100%;">
+          <div class="gradient-cards">
+            <div class="card">
+              <div class="container-card bg-green-box"
+                style="display: flex; align-items: center;justify-content: center; flex-wrap: wrap;">
+                <h1 class="card-title" style="width: 100%; font-size: 3rem;">Be Part of The IQ Jobs Family</h1>
+                <q-btn id="btn-early-access" unelevated rounded color="white" class="text-blue"
+                  style="margin-top: -4rem;" label="Early Access" @click="handleRedirect" />
+              </div>
+            </div>
           </div>
-
-          <div class="col-md-3 col-xs-12">
-            <q-card class="my-card text-white q-pa-none q-ma-none bg-green">
-              <q-card-section class="q-mb-lg q-pb-none">
-                <div class="text-h4 text-weight-bold text-white">New Features 🎖️</div>
-              </q-card-section>
-              <q-card-section class="q-pa-none q-ma-none">
-                <q-card-section v-for="(mytext, i) in mytexts" :key="i">
-                  <h6 class="q-my-none q-px-none text-white font-weight-bold titleCard">{{ mytext.title
-                    }}
-                  </h6>
-                  <div class="text-subtitle2 q-my-none q-px-none">{{ mytext.content }}</div>
-                  <q-separator size="1px" color="grey-5" class="q-mt-md q-pb-none q-mb-none"
-                    v-if="i < mytexts.length - 1" />
-                </q-card-section>
-              </q-card-section>
-            </q-card>
-          </div>
-
         </div>
-      </q-page>
-    </q-page-container>
-
-  </q-layout>
+      </q-page-container>
+    </q-layout>
+  </div>
 </template>
 
 <script setup>
 
-import { ref } from 'vue'
-
-const mytexts = ref([
-  {
-    title: 'Chat with Recruiters',
-    content: 'Lorem ipsun dolor sit amet consectetur adipisicing elit.!'
-  },
-  {
-    title: 'Import your profile from LinkedIn and Glassdoor',
-    content: 'What are the possible adverse effetcs of on-demand AI image generation?'
-  },
-  {
-    title: 'Create jobs with just a few clicks',
-    content: 'Private funding by VC firms is down 50% YOY. We take a look at what that means.'
-  }
-])
+const handleRedirect = () => {
+  window.location.href = '/';
+}
 
 </script>
 
-<style lang="scss" scoped>
-.q-page-container {
-  background-color: #fffffffc !important;
+<style lang="scss">
+body {
+  background: linear-gradient(#1976d2, #26a69a) !important;
 }
 
-.titleCard {
-  cursor: pointer;
-  margin-top: -15px;
+#btn-early-access:hover {
+  background-color: #1976d2;
+  color: white;
+}
 
-  &:hover {
-    color: $amber-4 !important;
+.container {
+  width: 1200px !important;
+  padding: 0 !important;
+  margin-right: auto;
+  margin-left: auto;
+
+  @media screen and (min-width: 992px) and (max-width: 1439px) {
+    max-width: 1279px !important;
+    padding: 0 !important;
+    margin: 0 80px !important;
+    width: auto !important;
+  }
+
+  @media screen and (max-width: 991px) {
+    max-width: 959px !important;
+    margin: 0 16px !important;
+    padding: 0 !important;
+    width: auto !important;
   }
 }
 
-.title-card-2 {
-  cursor: pointer;
+.gradient-cards {
+  display: flex;
+  gap: 32px;
+  padding: 30px;
+  height: 100%;
+  align-items: center;
 
-  &:hover {
-    color: rgb(203, 78, 78);
+  @media screen and (max-width: 991px) {
+    grid-template-columns: 1fr;
   }
 }
 
-.my-btn-color {
-  background-color: #141129;
+.container-title {
+  text-align: center;
+  padding: 0 !important;
+  margin-bottom: 40px;
+  font-size: 40px;
+  color: #fff;
+  font-weight: 600;
+  line-height: 60px;
 }
 
-.my-card {
-  max-width: 100%;
-  border-radius: none !important;
+.card {
+  max-width: 550px;
+  border: 0;
+  width: 100%;
+  margin-bottom: 5rem;
 }
 
-.my-card-2 {
-  width: 35%;
-}
+.container-card {
+  border: 2px solid rgb(187, 187, 200);
+  background: linear-gradient(71deg, #1976d2, #1c1c1c, #26a69a);
+  border-radius: 5px;
+  padding: 40px;
+  min-width: 70rem;
+  min-height: 20rem;
 
-.my-card-text-color {
-  color: #141129;
-
-  &:hover {
-    color: rgb(203, 78, 78) !important;
+  img {
+    margin-bottom: 32px;
   }
 }
 
-.q-card {
-  border-radius: 0 !important;
+.bg-green-box,
+.bg-white-box,
+.bg-yellow-box,
+.bg-blue-box {
+  position: relative;
 }
 
-.q-btn:hover {
-  background-color: rgb(203, 78, 78) !important;
+.bg-green-box::after,
+.bg-white-box::after,
+.bg-yellow-box::after,
+.bg-blue-box::after {
+  position: absolute;
+  top: -1px;
+  bottom: -1px;
+  left: -1px;
+  right: -1px;
+  content: "";
+  z-index: -1;
+  border-radius: 45px;
+}
+
+.bg-green-box::after {
+  background: linear-gradient(71deg, #0d1212, #3da077, #0d1212);
+}
+
+.bg-white-box::after {
+  background: linear-gradient(71deg, #121013, #b0afb0, #121013);
+}
+
+.bg-yellow-box::after {
+  background: linear-gradient(71deg, #110e0e, #afa220, #110e0e);
+}
+
+.bg-blue-box::after {
+  background: linear-gradient(71deg, #0c0a0e, #5f6fad, #0c0a0e);
+}
+
+.card-title {
+  font-weight: 600;
+  color: white;
+  letter-spacing: -0.02em;
+  line-height: 40px;
+  font-style: normal;
+  font-size: 28px;
+  padding-bottom: 8px;
+  text-align: center;
+}
+
+.card-description {
+  font-weight: 600;
+  line-height: 32px;
+  color: hsla(0, 0%, 100%, 0.5);
+  font-size: 16px;
+  max-width: 470px;
 }
 </style>
