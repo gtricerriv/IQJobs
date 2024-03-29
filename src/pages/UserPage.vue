906fac3acb39801231405eedb63eb7905c16a634
Cesar Febres <febrescesar7@gmail.com>
2024-03-28 20:42:14 -0400

    minifix

--- src/pages/UserPage.vue
+++ src/pages/UserPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="q-pa-md">
-    <q-table title="List Profile" flat bordered :rows="rows" :columns="columns" row-key="_id" :filter="filter"
-      :loading="loading" selection="single" v-model:selected="selected">
+    <q-table title="List Profile" flat bordered :rows="adminStore.usersList" :columns="columns" row-key="_id"
+      :filter="filter" :loading="loading" selection="single" v-model:selected="selected">
 
       <template v-slot:top>
         <div class="text-h6 text-grey q-pa-md">
@@ -57,116 +57,6 @@ const columns = [
   { name: 'createdAt', label: 'Created At', align: 'center', field: 'createdAt', sortable: true }
 ]
 
-const rows = [
-  {
-    _id: Date.now(),
-    email: 'example@iqjobs1.com',
-    profiles: [
-      {
-        _id: Date.now(),
-        title: 'example',
-        first_name: 'John',
-        last_name: 'Doe',
-        cover_letter: 'I am a programmer',
-        skills: 'Java, Python',
-        area: 'FrontEnd Developer',
-        education: 'Bachelor Degree',
-        experience: '5 years',
-        projects: 'Project 1, Project 2',
-        contact: '1234567890',
-        isRecruiter: false,
-        premium: false
-      },
-      {
-        _id: Date.now(),
-        title: 'example',
-        first_name: 'John',
-        last_name: 'Doe',
-        cover_letter: 'I am a backend programmer',
-        skills: 'PHP, Node',
-        area: 'Backend Developer',
-        education: 'Bachelor Degree',
-        experience: '1 years',
-        projects: 'Project A, Project B',
-        contact: '1234567890',
-        isRecruiter: false,
-        premium: false
-      }
-    ]
-  },
-  {
-    _id: Date.now() + 100,
-    email: 'example@iqjobs2.com',
-    profiles: [
-      {
-        _id: Date.now(),
-        title: 'example',
-        first_name: 'John2',
-        last_name: 'Doe2',
-        cover_letter: 'I am a programmer',
-        skills: 'Java, Python',
-        area: 'FrontEnd Developer',
-        education: 'Bachelor Degree',
-        experience: '5 years',
-        projects: 'Project 1, Project 2',
-        contact: '1234567890',
-        isRecruiter: false,
-        premium: false
-      },
-      {
-        _id: Date.now(),
-        title: 'example',
-        first_name: 'John2',
-        last_name: 'Doe2',
-        cover_letter: 'I am a backend programmer',
-        skills: 'PHP, Node',
-        area: 'Backend Developer',
-        education: 'Bachelor Degree',
-        experience: '1 years',
-        projects: 'Project A, Project B',
-        contact: '1234567890',
-        isRecruiter: false,
-        premium: false
-      }
-    ]
-  },
-  {
-    _id: Date.now() + 101,
-    email: 'example@iqjobs3.com',
-    profiles: [
-      {
-        _id: Date.now(),
-        title: 'example',
-        first_name: 'John3',
-        last_name: 'Doe3',
-        cover_letter: 'I am a programmer',
-        skills: 'Java, Python',
-        area: 'FrontEnd Developer',
-        education: 'Bachelor Degree',
-        experience: '5 years',
-        projects: 'Project 1, Project 2',
-        contact: '1234567890',
-        isRecruiter: false,
-        premium: false
-      },
-      {
-        _id: Date.now(),
-        title: 'example',
-        first_name: 'John3',
-        last_name: 'Doe3',
-        cover_letter: 'I am a backend programmer',
-        skills: 'PHP, Node',
-        area: 'Backend Developer',
-        education: 'Bachelor Degree',
-        experience: '1 years',
-        projects: 'Project A, Project B',
-        contact: '1234567890',
-        isRecruiter: false,
-        premium: false
-      }
-    ]
-  }
-]
 
 onMounted(async () => {
   $q.loading.show({
